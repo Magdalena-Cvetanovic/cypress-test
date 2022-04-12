16b6bcf07e093fc1f6d5fba674033f77fd2be16c
magdalenacvetanovic <magdalena.cvetanovic@outlook.com>
2022-04-12 10:03:15 +0200

    added new locator for dresses link

--- cypress/page-elements/home-page-elements.js
+++ cypress/page-elements/home-page-elements.js
@@ -1,4 +1,5 @@
 export default class HomePageElements{
     searchBar = '#search_query_top'
     dressesLink = '.sf-menu > :nth-child(2)> a'
+    dress = '[title="Dresses"]'
 }
--- cypress/pages/home-page.js
+++ cypress/pages/home-page.js
@@ -12,13 +12,17 @@ export default class HomePage extends Validaitons{
     getDressesLink(){
         return cy.get(this.pageElements.dressesLink)
     }
+    getDresseNav(){
+        return cy.get(this.pageElements.dress).eq(1)
+    }
     
     conductSearch(){
         this.getSearchBar().type(`${testData.search.criteria}{enter}`)
     }
 
     clickDressesLinkAndValidate(item){
-        this.getDressesLink().click()
+        // this.getDressesLink().click()
+        this.getDresseNav().click()
         this.validateTheUserIsOnTheCorrectPage(testData.pageNavigation.dressPage)
         this.validateTheCartHasItems(item)
     }
